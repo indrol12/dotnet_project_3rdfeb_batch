fe0e35d16f8752ee29808764a509bf0f48a5c00c
indrol12 <indrolofficial@gmail.com>
2026-03-04 14:00:20 +0530

    all files of filehandling and multithreading are committed

---
+++ Csharp_programs_feb/binaryfile_handling.cs
@@ -0,0 +1,39 @@
+﻿
+//     class binaryfile_handling
+//    {
+//       void write_data()
+//      {
+//        string path = "C:\\Users\\FortuneCloudIndia\\Desktop\\user.bin";
+//       FileStream fs=new FileStream(path, FileMode.OpenOrCreate);
+//        BinaryWriter bw=new BinaryWriter(fs);
+//        bw.Write(556);
+//        bw.Write("hello everyone");
+//        bw.Write(89.1);
+//        bw.Write(true);
+//        bw.Close();
+//        fs.Close();
+//    }
+//    void read_data()
+//    {
+//        string path = "C:\\Users\\FortuneCloudIndia\\Desktop\\user.bin";
+//        FileStream fs2 = new FileStream(path, FileMode.Open);
+//        BinaryReader br=new BinaryReader(fs2);
+//        int num=br.ReadInt32();
+//        string str=br.ReadString();
+//        double d=br.ReadDouble();
+//        bool b=br.ReadBoolean();
+
+//        Console.WriteLine("integer:"+num);
+//        Console.WriteLine("String:" + str);
+//        Console.WriteLine("Double:"+d);
+//        Console.WriteLine("boolean:"+b);
+//        br.Close();
+//        fs2.Close();
+//    }
+//    public static void Main(string[] args)
+//    {
+//        binaryfile_handling ob=new binaryfile_handling();
+//        ob.write_data();
+//        ob.read_data();
+//    }
+//}
---
+++ Csharp_programs_feb/multithreading.cs
@@ -0,0 +1,23 @@
+﻿
+class multithreading//1.create class
+{
+      void show()//2.create task for thread
+
+    { 
+        Console.WriteLine("Thread is executing");
+        }
+     void display()
+    {
+        Console.WriteLine("2nd thread is running");
+    }
+    public static void Main(string[] args)
+    {
+        multithreading ob1=new multithreading();
+
+        Thread th1=new Thread(ob1.show);//3.create the instance of thread class
+        Thread th2 = new Thread(ob1.display);
+        th1.Start();//4.call start method
+        th2.Start();
+    }
+
+}
